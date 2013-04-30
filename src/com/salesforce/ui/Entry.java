2f29855bb6ccedb3439c114d3e13c0894db64abf
Yuchengw <ycwmike@gmail.com>
2013-04-29 18:11:46 -0700

    finish at home

---
+++ src/com/salesforce/ui/Entry.java
@@ -0,0 +1,24 @@
+/**
+ * The program that performs as the entry of the program
+ *
+ */
+
+package com.salesforce.ui;
+
+
+import com.salesforce.factroy.ToolFactory;
+import com.slaesforce.factory.MigrateFactory;
+import com.salesforce.ui.MappingParser;
+import com.salesforce.ui.MappingBean;
+import com.salesforce.service.MappingBean;
+
+public class Entry{
+	
+	public Entry(){
+	}
+	
+	public static void main(String[] args){
+
+	}
+
+}
