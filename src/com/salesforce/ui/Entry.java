df1c1d70804128c75db015a0d74d620cae62ea8b
Yucheng Wang <yucheng.wang@yuchengwang-ltm.local>
2013-04-30 17:29:15 -0700

    finish entry.java in desktop

--- src/com/salesforce/ui/Entry.java
+++ src/com/salesforce/ui/Entry.java
@@ -1,5 +1,6 @@
 /**
  * The program that performs as the entry of the program
+ * The main class of program
  *
  */
 
@@ -13,12 +14,59 @@ import com.salesforce.ui.MappingBean;
 import com.salesforce.service.MappingBean;
 
 public class Entry{
+
+	// Default values
+	boolean VERBOSE_MODE=false;
+	boolean INTERACTIVE_MODE=false;
 	
 	public Entry(){
 	}
-	
-	public static void main(String[] args){
+		
+    /**
+     * main function that deal with user's input 
+     * @param args input user string
+     * @return void
+     */
+	public static void parseArgument(String[] args){
+		for(String arg : args){
+			if(arg.toLowerCase().contains("-v") || arg.toLowerCase().contains("--verbose")){
+				VERBOSE_MODE=true; // output log info
+			}
+			if(arg.toLowerCase().contains("-i") || arg.toLowerCase().contains("--interactive")){
+				INTERACTIVE_MODE=true; // interactively generate xml file
+			}
+		}
+		if(VERBOSE_MODE) enableLog();
+		if(INTERACTIVE_MODE) enableCLI(args);
+    }
 
+	/** TODO:
+     * The function that direct log output to console 
+     *
+     */
+	public static void enableLog(){
+
+	}
+
+	/**
+     * The function that enable Command line tool parse user input and generate XML config file
+     *
+     */
+	public static void enableCLI(String[] args){
+
+	}
+	/**
+     * The function that deales with no user input configure inforation
+     *
+     */
+	public static void disableCLI(){
+		
+	}
+	public static void main(String[] args) throws FileNotFoundException, AsynsException{
+		System.out.println("******************Starting TOOL********************");
+		parserArgument(args);
+		// if user has not implement CLI mode, use already configured XML file
+		if(!INTERACTIVE_MODE) disableCLI();
 	}
 
 }
