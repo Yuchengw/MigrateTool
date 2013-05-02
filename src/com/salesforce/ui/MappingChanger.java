f5eed6c8d75b2396188bb255dd866f2d9634d4cb
Yucheng Wang <yucheng.wang@yuchengwang-ltm.local>
2013-05-01 21:55:12 -0700

    prepare to write test file

--- src/com/salesforce/ui/MappingChanger.java
+++ src/com/salesforce/ui/MappingChanger.java
@@ -17,12 +17,13 @@ import java.io.*;
 import java.util.ArrayList;
 
 
-public class MappingExchanger{
+public class MappingChanger{
 
 	private MappingBean mb;
 	private ArrayList<String> fromorglist;	
 	private ArrayList<String> toorglist;
-	public MappingParser(){
+
+	public MappingChanger(){
 		System.out.println("***************Start Chang*******************");
 	}
 
@@ -32,19 +33,20 @@ public class MappingExchanger{
 	 * @param mappingbean
      * @return void
      * @throws FileNotFoundException
+     * @throws IOException
      */
-	public void change(String filename, MappingBean mb) throws FileNotFoundException{
+	public void change(String filename, MappingBean mb) throws FileNotFoundException, IOException{
 		File changingfile = new File(filename);
 		if(!changingfile.exists()){
-			throw new FileNotFoundException("****" + filename + "****" + is not found);
+			throw new FileNotFoundException("****" + filename + "**** is not found");
 		}
 		if(mb == null){
 			System.out.println("MappingBean is Null");
 			return;
 		}
-		formorglist = mb.getFromOrgFields().clone();
-		toorglist   = mb.getToOrgFields().clone();
-		return changeImpl(filename, fromorglist, toorglist);
+		fromorglist = (ArrayList<String>)mb.getFromList().clone();
+		toorglist   = (ArrayList<String>)mb.getToList().clone();
+		changeImpl(changingfile, filename, fromorglist, toorglist);
 	}
 	/**
      * Function that implements the core function that change exsiting csv file 
@@ -52,12 +54,14 @@ public class MappingExchanger{
      * @param filename 
      * @param fromorglist
 	 * @param toorglist
+	 * @throws IOException
      * @return void
      */
-	public void changeImpl(File infile, String filename,ArrayList<String> fromorglist, ArrayList<String> toorglist){
-		BufferedReader rdr = new BufferedReader(new InputStreamReader(new FileInputStream(csvFileName)));
+	public void changeImpl(File infile,String filename, ArrayList<String> fromorglist, ArrayList<String> toorglist) throws IOException{
+		FileInputStream instream = new FileInputStream(infile);
+		BufferedReader rdr = new BufferedReader(new InputStreamReader(instream));
 		File outfile = new File(filename + ".tmp");
-		BUfferedWriter  wt = new BufferedWriter(new FileOutputStream(new File(outfile)));
+		FileOutputStream outstream = new FileOutputStream(outfile);	
 		StringBuilder fieldsname = new StringBuilder();
 		ArrayList<String> checklist = new ArrayList<String>();
 		// yeah, it could be a buggy place, let's see 
@@ -65,20 +69,22 @@ public class MappingExchanger{
 			fieldsname.append(rdr.readLine());
 		}
 		String[] line = fieldsname.toString().split(",");
-		for(int i = 0; i < line1; i++){
+		for(int i = 0; i < line.length; i++){
 			System.out.println("  " + line[i]);
 			checklist.add(line[i]);	
 		}
-		if(isEqual(checklist,fromorglist)) String newline = createCSV(checklist);
-		String line = null;
-		wt.write(newline);
-		while((line = rdr.readLine()) != null){
-			wt.write(line);
+		String newline = null;
+		if(isEqual(checklist,fromorglist)){
+			newline = createCSV(checklist);
 		}
-		rdr.flush();
-		rdr.close();
-		wt.flush();
-		wt.close();
+		String strline = null;
+		outstream.write(newline.getBytes("UTF-8"));
+		while((strline = rdr.readLine()) != null){
+			outstream.write(strline.getBytes("UTF-8"));
+		}
+		instream.close();
+		outstream.flush();
+		outstream.close();
 
 		if(!infile.delete() || !outfile.renameTo(infile)){
 			System.out.println("INFO:++++++++++++++++++++++File operation error");
@@ -95,7 +101,7 @@ public class MappingExchanger{
 		if(list1 == null || list2 == null || list1.size() != list2.size()){
 			return false;
 		}
-		for(int i = 0; i < list1; i++){
+		for(int i = 0; i < list1.size(); i++){
 			if(!list1.get(i).equals(list2.get(i))) return false;
 		}		
 		return true;	
@@ -107,9 +113,9 @@ public class MappingExchanger{
      *
      */
 	public String createCSV(ArrayList<String> list){
-	   String newstr;
+	   String newstr = null;
 	   for(int i = 0; i< list.size(); i++){
-			if( ++i == size()){
+			if( ++i == list.size()){
 				newstr = newstr + list.get(i) + "\n";	
 				break;
 			}
