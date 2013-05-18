d10044af60e7aadc9a2e2d05d3c8511c9e12f35e
Yuchengw <ycwmike@gmail.com>
2013-05-17 17:00:11 -0700

    finish

---
+++ src/com/salesforce/service/lib/Log/SuperLog.java
@@ -0,0 +1,25 @@
+/**
+ * Super log template
+ *
+ *
+ */
+
+
+package com.salesforce.service.lib.log;
+
+
+import java.util.Calendar;
+
+
+public class SuperLog{
+
+	private SuperLog(){
+		throw new UnsupportedOperationException();
+	}
+	//TODO: finish the warper methods 
+
+
+
+}
+
+
